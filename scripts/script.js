f154b08bb2cbb8ddfe96fe419e7f6f77800607a1
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-02-05 13:45:59 -0800

    1:45PM-2/5/23-LucasC

--- scripts/script.js
+++ scripts/script.js
@@ -80,7 +80,7 @@ for (job of data) {
 }
 
 function search_job() {
-    let input = document.getElementById('searchbar').value
+    let input = document.getElementById('searchbar').value;
     input=input.toLowerCase();
     let x = document.getElementsByClassName('job');
     for (i = 0; i < x.length; i++) { 
@@ -98,44 +98,73 @@ function search_job() {
     }
 }
 
-var GrossIncome = 46200;
-if (typeof GrossIncome == 'number') {
-    var GrossMonthly = GrossIncome / 12;
-    var HousingAffordability = GrossMonthly  * 0.33;
-} else {
-    console.log("Error! Please input number into field.");
-}
-
-var FederalTaxes = GrossMonthly * 0.12;
-var StateTaxes = GrossMonthly * 0.07;
-var SocialSecurity = GrossMonthly * 0.062;
-var Medicare = GrossMonthly * 0.0145;
-var StateDisability = GrossMonthly * 0.01;
-var RetirementInvestment = GrossMonthly * 0.05;
-var MedicalInsurance = 180;
-var TotalDeductions = GrossMonthly - MedicalInsurance;
-var NetMonthly = GrossMonthly - TotalDeductions;
-var CurrentBalance = NetMonthly;
-console.log(HousingAffordability);
-
-var TransactionDescription = "Kraft Mac";
-if (typeof TransactionDescription == 'string') {
-    if (TransactionDescription.length < 32) {
-        var TransactionType = "+";
-        var TransactionAmount = 1000;
-        if (typeof (TransactionAmount) == "number") {
-                    // CurrentBalance = +/- TransactionAmount
-                    // Output CurrentBalance
-                    // Last Item in List?
-                    // Yes, End
-                    // No, Repeat
-                    document.getElementById("CurrentBalance").innerHTML = (levemealon);
-        } else {
-            console.log("Error! Please enter number into field.")
+function example() {
+    let numInput = documnet.getElementById('grossInput').value;
+    numInput=numInput.toLowerCase();
+    let y = document.getElementsByClassName('labelFlex');
+    let ychild = document.getElementsByClassName('areaFlex');
+    for(i = 0; i < y.length; i++) {
+        if (!y[i].innerHTML.toLowerCase().includes(numInput)) {
+            y[i].style.display="none";
         }
-    } else {
-        console.log("Error! Too many characters.")
+        else {
+            y[i].style.display="flex";
+        }
     }
-} else {
-    console.log("Error! Please enter non-numerical characters into field.")
+    if (numInput == "") {
+        for (ychild of y) {
+            ychild.style.display="none";
+        }
+    }
 }
+
+// function deductionCalc() {
+//     let AnnualIncome = document.getElementById('grossInput').value;
+//     while (AnnualIncome == 'number') {
+//             let GrossMonthly = AnnualIncome / 12;
+//             let HousingAffordability = GrossMonthly * 0.33;
+//             document.getElementById('GrossMonthly').innerHTML.innerText = GrossMonthly;
+//     }
+// }
+
+// var GrossIncome = 46200;
+// if (typeof GrossIncome == 'number') {
+//     var GrossMonthly = GrossIncome / 12;
+//     var HousingAffordability = GrossMonthly  * 0.33;
+// } else {
+//     console.log("Error! Please input number into field.");
+// }
+
+// var FederalTaxes = GrossMonthly * 0.12;
+// var StateTaxes = GrossMonthly * 0.07;
+// var SocialSecurity = GrossMonthly * 0.062;
+// var Medicare = GrossMonthly * 0.0145;
+// var StateDisability = GrossMonthly * 0.01;
+// var RetirementInvestment = GrossMonthly * 0.05;
+// var MedicalInsurance = 180;
+// var TotalDeductions = GrossMonthly - MedicalInsurance;
+// var NetMonthly = GrossMonthly - TotalDeductions;
+// var CurrentBalance = NetMonthly;
+// console.log(HousingAffordability);
+
+// var TransactionDescription = "Kraft Mac";
+// if (typeof TransactionDescription == 'string') {
+//     if (TransactionDescription.length < 32) {
+//         var TransactionType = "+";
+//         var TransactionAmount = 1000;
+//         if (typeof (TransactionAmount) == "number") {
+//                     // CurrentBalance = +/- TransactionAmount
+//                     // Output CurrentBalance
+//                     // Last Item in List?
+//                     // Yes, End
+//                     // No, Repeat
+//                     document.getElementById("CurrentBalance").innerHTML = (levemealon);
+//         } else {
+//             console.log("Error! Please enter number into field.")
+//         }
+//     } else {
+//         console.log("Error! Too many characters.")
+//     }
+// } else {
+//     console.log("Error! Please enter non-numerical characters into field.")
+// }
